9c23cd32dee595b91b61106072f7a204279fa467
Maxime Boucher <mxboucher@gmail.com>
2024-11-11 22:23:22 -0800

    tests: add faker data matching requirements

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+URLS = [
+  'http://apple.com', 'https://apple.com', 'https://www.apple.com',
+  'http://developer.apple.com', 'http://en.wikipedia.org', 'http://opensource.org',
+  Faker::Internet.url
+]
+
+REFERRERS = [
+  'http://apple.com', 'https://apple.com', 'https://www.apple.com',
+  'http://developer.apple.com', nil, Faker::Internet.url
+]
+
+1_000_000.times do
+  PageView.create!({
+                     url: URLS.sample,
+                     referrer: REFERRERS.sample,
+                     viewed_at: Faker::Time.between(from: 14.days.ago, to: Time.now)
+                   })
+  # Add batch size for performance, e.g., every 1000 records
+  if PageView.count % 1000 == 0
+    puts "Inserted #{PageView.count} records."
+  end
+end
